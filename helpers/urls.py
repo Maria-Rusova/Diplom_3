68dd08d9eb4d19f3f7c81831832008df828437a4
Mariya R <KapitanProMashka@yandex.ru>
2026-04-27 22:11:08 +0300

    update README.md

---
+++ helpers/urls.py
@@ -0,0 +1,6 @@
+class Url:
+    BASE_URL = 'https://stellarburgers.education-services.ru'
+
+    FEED_URL = f'{BASE_URL}/feed'
+    CREATE_USER = f'{BASE_URL}/api/auth/register'
+    DELETE_USER = f'{BASE_URL}/api/auth/user'
